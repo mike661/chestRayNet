43cb34b4d41caf98ff4326abeb4c68916335bd69
mike661 <michael661@gmail.com>
2022-02-08 12:27:39 +0000

    train modifications

--- train.py
+++ train.py
@@ -6,53 +6,78 @@ import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau
 from tensorflow.keras.optimizers import Adam
 import os
+import pandas as pd
+import numpy as np
 
 from tensorflow.keras.layers import Dense
 
+
 def main():
 
-    #DIRS
+    # DIRS
     root = './experiment/'
-    experiment_dir = os.path.join(root, '1')
+    experiment_path = os.path.join(root, '1')
 
-    output_weights_path = os.path.join(experiment_dir, 'best_weights.h5')
+    output_weights_path = os.path.join(experiment_path, 'best_weights.h5')
 
     CSV_PATH = './data_split/'
     train_path = os.path.join(CSV_PATH, 'train.csv')
     val_path = os.path.join(CSV_PATH, 'val.csv')
     test_path = os.path.join(CSV_PATH, 'test.csv')
 
+    batch_size = 32  # total samples / batch_size / 10)
+    steps =  # total samples / batch_size / 5)
 
-    batch_size = 32 #total samples / batch_size / 10)
-    steps =  #total samples / batch_size / 5)
-
-
+    train_pd = pd.read_csv(train_path)
+    val_pd = pd.read_csv(val_path)
 
     if not os.path.isdir(experiment_dir):
         os.makedirs(experiment_dir)
 
-
-    #TODO checkpoint
+    # TODO checkpoint
     checkpoint = ModelCheckpoint(
-                    output_weights_path,
-                    monitor="val_loss",
-                    save_weights_only=True,
-                    save_best_only=True,
-                    verbose=1,
-                )
+        output_weights_path,
+        monitor="val_loss",
+        save_weights_only=True,
+        save_best_only=True,
+        verbose=1,
+    )
 
     callbacks = [
-            checkpoint,
-            #TensorBoard(log_dir=os.path.join(output_dir, "logs"), batch_size=batch_size),
-            ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=1,
-                              verbose=1, mode="min", min_lr=1e-8),
-            #auroc,
-        ]
+        checkpoint,
+        #TensorBoard(log_dir=os.path.join(output_dir, "logs"), batch_size=batch_size),
+        ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=1,
+                          verbose=1, mode="min", min_lr=1e-8),
+        # auroc,
+    ]
 
-    tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255.)
-    tf.keras.preprocessing.image.ImageDataGenerator()
+    train_data_gen = tf.keras.preprocessing.image.ImageDataGenerator(horizontal_flip=True, preprocessin_function=tf.keras.applications.densenet.preprocess_input)
+    val_data_gen = tf.keras.preprocessing.image.ImageDataGenerator()
 
-    img_input = tf.keras.Input(shape=(224,224,3))
+    label_names = ['Atelectasis','Cardiomegaly','Effusion','Infiltration','Mass','Nodule','Pneumonia','Pneumothorax','Consolidation','Edema','Emphysema','Fibrosis','Pleural_Thickening','Hernia']
+
+    train_generator = train_data_gen.flow_from_dataframe(
+        dataframe=train_pd,
+        directory="./images",
+        x_col="Image Index",
+        y_col=label_names,
+        batch_size=32,
+        shuffle=True,
+        class_mode="raw",
+        target_size=(224, 224),
+        )
+
+    val_generator = val_data_gen.flow_from_dataframe(
+        dataframe=val_pd,
+        directory="./images",
+        x_col="Image Index",
+        y_col=label_names,
+        batch_size=32,
+        shuffle=False,
+        class_mode="raw",
+        target_size=(224, 224))
+
+    img_input = tf.keras.Input(shape=(224, 224, 3))
 
     optimizer = Adam(lr=0.001)
 
@@ -61,7 +86,7 @@ def main():
     base_exec_prefix = tf.keras.applications.DenseNet121(
         include_top=False,
         input_tensor=img_input,
-        input_shape=(224,224,3),
+        input_shape=(224, 224, 3),
         weights='imagenet,
         pooling="avg")
 
@@ -70,7 +95,9 @@ def main():
 
     model = tf.keras.Model(inputs=img_input, outputs=predictions)
 
-    model.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=[auc, 'accuracy'])
+    model.compile(loss="binary_crossentropy",
+                  optimizer=optimizer, metrics=[auc, 'accuracy'])
+
 
 if __name__ == "__main__":
     main()
